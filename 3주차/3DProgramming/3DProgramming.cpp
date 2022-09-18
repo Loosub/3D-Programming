e7244a89ecab1d72542fe3e90c388874e9d04705
Loosub <vytmd0204@gmail.com>
2022-09-19 00:34:55 +0900

    [PSH] 3주차, line strip으로 반지름 1인 원 그리기
    -> GL_LINE_STRIP: 연결된 선분을 잇지만 처음과 끝점이 만나지 않음
    -> 처음 지점에서 다음 지점(x,y,z)로 선분 이음
    -> x = radius * sin(angle)
    -> y = radius * cos(angle)

---
+++ 3주차/3DProgramming/3DProgramming.cpp
@@ -0,0 +1,73 @@
+﻿#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+#pragma comment(lib, "OpenGL32")
+
+const float steps = 100;
+const float angle = 3.1415926 * 2.0f / steps;
+
+static void error_callback(int error, const char* description)
+{
+    fputs(description, stderr);
+}
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+int main(void)
+{
+    GLFWwindow* window;
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+    window = glfwCreateWindow(640, 640, "Simple example", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    float x, y, z, angle = 0;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float ratio;
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float)height;
+
+        ////90도와 같이 각도로 연산하고 싶으면
+        ////glm의 삼각함수는 radian 형식으로 입력을 받기 때문에 변환해서 넣어주어야한다.
+        //glm::sin(glm::radians(90.0));  
+        
+        glClearColor(0.0f, 0.0f, 0.0f, 0.1f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_LINE_STRIP);
+
+        for (angle = 0.0f; angle <= (2.0f * 3.14156f) * 1.01f; angle += 0.01f) {
+            x = 1.0f * sin(angle);
+            y = 1.0f * cos(angle);
+            z = 0.0f;
+            glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+            glVertex3f(x, y, z);
+        }
+
+        glEnd();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    
+    exit(EXIT_SUCCESS);
+}
